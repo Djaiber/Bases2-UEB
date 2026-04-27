1af567a80356f36db8855eecb4e9626ea47a1c76
jaiber diaz <djaiver9@gmail.com>
2026-04-27 15:26:32 -0500

    primer insercion many en mongo

---
+++ miprimerscripts.js
@@ -0,0 +1,7 @@
+use ("mudial2026")
+
+db.selecciones.insertMany([
+    { nombre: "Argentina", colorCamiseta: "celeste y blanca", capital: "Buenos Aires", poblacion: 45000000 },
+    { nombre: "Brasil", capital: "Brasilia", mundialesGanados: 5 },
+    { nommbre: "Republica del Congo", edadPromedio:15, continente: "Africa" }
+])
